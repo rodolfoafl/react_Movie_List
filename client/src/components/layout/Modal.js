34a58328c0487365bfbfca855fb9780f8878d52d
Rodolfo <rodolfoafl@gmail.com>
2020-04-07 21:01:05 -0300

    Updated Modal CSS

--- client/src/components/layout/Modal.js
+++ client/src/components/layout/Modal.js
@@ -21,21 +21,31 @@ const Modal = ({ show, onClose, children }) => {
     <Fragment>
       {!show ? null : (
         <Fragment>
-          <div className="modal text-primary" id="modal">
-            <h2>{children}</h2>
-            {movieLists.map((l) => (
-              <div className="modal-content text-primary">
-                <h3>{l.name}</h3>
-              </div>
-            ))}
+          <div className="modal-background">
+            <div className="modal text-dark" id="modal">
+              <h3>{`Adicionar ${children} à:`}</h3>
+              {movieLists.map((l) => (
+                <div className="modal-content text-dark" key={l.name}>
+                  <label htmlFor={l.name}>
+                    {l.name}
+                    <input
+                      type="checkbox"
+                      className="mx-1"
+                      id={l.name}
+                      name={l.name}
+                    ></input>
+                  </label>
+                </div>
+              ))}
 
-            <div className="modal-actions">
-              <button
-                className="toggle-button"
-                onClick={(e) => onCloseModal(e)}
-              >
-                Close
-              </button>
+              <div className="modal-actions">
+                <button
+                  className="toggle-button"
+                  onClick={(e) => onCloseModal(e)}
+                >
+                  Fechar
+                </button>
+              </div>
             </div>
           </div>
         </Fragment>
