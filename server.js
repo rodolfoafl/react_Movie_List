e439fe26ff8111e06f6990e046a271d8d562161a
Rodolfo <rodolfoafl@gmail.com>
2020-04-12 16:50:55 -0300

    Updated server.js

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get("/", (req, res) => res.json({ msg: "Welcome..." }));
 //Define Routes
 app.use("/api/lists", require("./routes/lists"));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port  ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
